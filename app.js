8b62c47884aef193ddd33997d3a43ef815bc49a8
Shereal McNeal <opcommander1@Shereal-McNeals-MacBook-Pro.local>
2018-11-27 21:33:40 -0600

    add views and landing page testing

---
+++ app.js
@@ -0,0 +1,22 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    flash           = require("connect-flash"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    methodOverride  = require("method-override")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+app.get("/", function(req, res){
+  res.render("landing")
+});
+
+app.listen('8888', function(){
+  console.log("Server has stated");
+});
