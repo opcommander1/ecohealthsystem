d2059a5e4f692ed417050e207e6fe639ccaa1a81
Shereal McNeal <opcommander1@Shereal-McNeals-MacBook-Pro.local>
2018-12-28 22:23:13 -0600

    switch back to local mysql

--- app.js
+++ app.js
@@ -29,21 +29,14 @@ app.use(methodOverride("_method"));
 app.use(flash());
 app.use(expressValidator());
 
-// var options = {
-//    host:          'localhost',
-//    port:          3306,
-//    localAddress:  '127.0.0.1',
-//    socketPath:    '/Applications/MAMP/tmp/mysql/mysql.sock',
-//    user:          'root',
-//    password:      'root',
-//    database:      'ecosystem'
-// };
-
 var options = {
-   host:          'mysql.op1commander.com',
-   user:          'op1commander',
-   password:      'fleetofficer50',
-   database:      'ecohealth'
+   host:          'localhost',
+   port:          3306,
+   localAddress:  '127.0.0.1',
+   socketPath:    '/Applications/MAMP/tmp/mysql/mysql.sock',
+   user:          'root',
+   password:      'root',
+   database:      'ecosystem'
 };
 
 var sessionStore = new MySQLStore(options);
--- config/config.js
+++ config/config.js
@@ -1,22 +1,17 @@
-var host = 'mysql.op1commander.com';
-var user = 'op1commander';
-var password = 'fleetofficer50';
-var database = 'ecohealth';
-
-// var host = 'localhost';
-// var port = 3306;
-// var localAddress = '127.0.0.1';
-// var socketPath = '/Applications/MAMP/tmp/mysql/mysql.sock';
-// var user = 'root';
-// var password = 'root';
-// var database = 'ecosystem';
+var host = 'localhost';
+var port = 3306;
+var localAddress = '127.0.0.1';
+var socketPath = '/Applications/MAMP/tmp/mysql/mysql.sock';
+var user = 'root';
+var password = 'root';
+var database = 'ecosystem';
 
 //database connectivity
 module.exports = {
     host,
-    // port,
-    // localAddress,
-    // socketPath,
+    port,
+    localAddress,
+    socketPath,
     user,
     password,
     database,
